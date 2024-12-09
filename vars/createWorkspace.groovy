e4c0de040c6d78dea4dba779a0031f627dbbe0a4
SreeVeerDevOps <47476473+SreeVeerDevOps@users.noreply.github.com>
2024-12-09 07:33:02 +0530

    new

--- vars/createWorkspace.groovy
+++ vars/createWorkspace.groovy
@@ -1,5 +1,5 @@
 def call(String workspace = 'dev') {
   echo "Creating Terraform ${workspace} Workspaces...."
   sh "terraform version"
-  sh "terraform workspace create ${workspace} || terraform workspace select ${workspace}"
+  sh "terraform workspace new ${workspace} || terraform workspace select ${workspace}"
 }
