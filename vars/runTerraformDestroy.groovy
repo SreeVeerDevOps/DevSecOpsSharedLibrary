a8284ab9021157b39f487c157b168cdf6e15da45
SreeVeerDevOps <47476473+SreeVeerDevOps@users.noreply.github.com>
2024-12-09 08:04:50 +0530

    as

--- vars/runTerraformDestroy.groovy
+++ vars/runTerraformDestroy.groovy
@@ -1,7 +1,6 @@
 def call(String tfvars = 'dev.tfvars', String letsdestroy = false) {
   echo "Running Terraform Code...."
   sh "terraform version"
-  sh "terraform init && terraform fmt && terraform validate"
   sh """
   if (letsdestroy=true)
   then
