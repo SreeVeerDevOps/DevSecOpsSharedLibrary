79759ebec2e9721ce2db8f6b4af60f629447a669
SreeVeerDevOps <47476473+SreeVeerDevOps@users.noreply.github.com>
2024-12-09 07:49:39 +0530

    -reconfigure

--- vars/createWorkspace.groovy
+++ vars/createWorkspace.groovy
@@ -1,6 +1,6 @@
 def call(String workspace = 'dev') {
   echo "Creating Terraform ${workspace} Workspaces...."
   sh "terraform version"
-  sh "terraform init"
+  sh "terraform init -reconfigure"
   sh "terraform workspace new ${workspace} || terraform workspace select ${workspace}"
 }
